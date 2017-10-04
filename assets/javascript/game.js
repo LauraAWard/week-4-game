3c140904d889be54f7aca35d35636b470cb15abd
LauraAWard <l-ward@northwestern.edu>
2017-10-04 00:26:06 -0500

    working 10-4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,7 @@ var imgArray = [
 	"assets/images/amethyst.jpg",
 	"assets/images/yellow.jpg",
 	"assets/images/green.jpg",
-	"assets/images/purple.jpg",
-	"assets/images/blue.jpg",
-	"assets/images/pink.jpg",
-	"assets/images/rainbow.jpg",
-	"assets/images/rainbow2.jpg",
-	"assets/images/white.jpg"];
+	"assets/images/purple.jpg"];
 var gemCount = 4;
 var idRoot = "crystal-";
 var crystalArray = [];
@@ -44,22 +39,12 @@ function crystal(id, src) {
 	};
 };
 
-// function createCrystalArray() {
-
-// 	 var crystal1 = new crystal("crystal-1", "assets/images/amethyst.jpg");
-// 	 var crystal2 = new crystal("crystal-2", "assets/images/yellow.jpg");
-// 	 var crystal3 = new crystal("crystal-3", "assets/images/green.jpg");
-// 	 var crystal4 = new crystal("crystal-4", "assets/images/purple.jpg");
-// 	 crystalArray.push(crystal1, crystal2, crystal3, crystal4);
-// };
-
 function createCrystalArray() {
 	
 	var indexCnt, idCnt;
 	
 	for (indexCnt = 0, idCnt = (indexCnt + 1); indexCnt < gemCount; indexCnt++, idCnt++) {
 		var tempID = (idRoot + idCnt);
-		var tempIndex = indexCnt;
 		var tempImg = imgArray[indexCnt];
 		crystalArray[indexCnt] = new crystal(tempID, tempImg);
 	}
@@ -70,7 +55,7 @@ function displayCrystals() {
    	var crystalDiv = $("#crystalBtns");
  
   	$.each(crystalArray, function(number, crystal){
-  		crystalDiv.append("<div><button type='button' class='btn' id=" + this.getID() + 
+  		crystalDiv.append("<div col-md-3><button type='button' class='btn' id=" + this.getID() + 
   		" value=" + this.getvalue() + "><img src=" + this.getSrc() + "></button></div>");
   	 });
  };
@@ -98,20 +83,26 @@ function resetScore() {
 
 function compareNumbers() {
 	
-	gameOver = false;
-
 	if (totalScore > randomNumber) {
-		gameLosses++;
-		gameOver = true;
-		$("#lossCnt").html(gameLosses);
-		resetGame();
+		gameLost();
 	}
 	else if (totalScore === randomNumber) {
-		gameWins++;
-		gameOver = true;
-		$("#winCnt").html(gameWins);
-		resetGame();
+		gameWon();
 	}
+};
+
+function gameLost() {
+	gameLosses++;
+	gameOver = true;
+	$("#lossCnt").html(gameLosses);
+	resetGame();
+};
+
+function gameWon() {
+	gameWins++;
+	gameOver = true;
+	$("#winCnt").html(gameWins);
+	resetGame();
 };
 
 function resetCrystalValues() {
@@ -136,6 +127,7 @@ function resetGame() {
 	resetCrystalValues();
 	setRandomNumber();
 	resetScore();
+	gameOver = false;
 };
 
 	startGame();
