678035f06b5f636216458f3d7cbef26ac498aaa4
LauraAWard <l-ward@northwestern.edu>
2017-10-03 20:13:46 -0500

    working 10-3b

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,17 @@
 $(window).on( "load", function() { 
 
-
-/*var crystal1 = 0;
-var crystal2 = 0;
-var crystal3 = 0;
-var crystal4 = 0;*/
+var imgArray = [
+	"assets/images/amethyst.jpg",
+	"assets/images/yellow.jpg",
+	"assets/images/green.jpg",
+	"assets/images/purple.jpg",
+	"assets/images/blue.jpg",
+	"assets/images/pink.jpg",
+	"assets/images/rainbow.jpg",
+	"assets/images/rainbow2.jpg",
+	"assets/images/white.jpg"];
+var gemCount = 4;
+var idRoot = "crystal-";
 var crystalArray = [];
 var randomNumber = 0;
 var totalScore = 0;
@@ -37,30 +44,45 @@ function crystal(id, src) {
 	};
 };
 
-function createCrystalArray() {
+// function createCrystalArray() {
 
-	 var crystal1 = new crystal("crystal-1", "assets/images/amethyst.jpg");
-	 var crystal2 = new crystal("crystal-2", "assets/images/yellow.jpg");
-	 var crystal3 = new crystal("crystal-3", "assets/images/green.jpg");
-	 var crystal4 = new crystal("crystal-4", "assets/images/purple.jpg");
-	 crystalArray.push(crystal1, crystal2, crystal3, crystal4);
+// 	 var crystal1 = new crystal("crystal-1", "assets/images/amethyst.jpg");
+// 	 var crystal2 = new crystal("crystal-2", "assets/images/yellow.jpg");
+// 	 var crystal3 = new crystal("crystal-3", "assets/images/green.jpg");
+// 	 var crystal4 = new crystal("crystal-4", "assets/images/purple.jpg");
+// 	 crystalArray.push(crystal1, crystal2, crystal3, crystal4);
+// };
+
+function createCrystalArray() {
+	
+	var indexCnt, idCnt;
+	
+	for (indexCnt = 0, idCnt = (indexCnt + 1); indexCnt < gemCount; indexCnt++, idCnt++) {
+		var tempID = (idRoot + idCnt);
+		var tempIndex = indexCnt;
+		var tempImg = imgArray[indexCnt];
+		crystalArray[indexCnt] = new crystal(tempID, tempImg);
+	}
 };
 
 function displayCrystals() {
+   	
    	var crystalDiv = $("#crystalBtns");
  
   	$.each(crystalArray, function(number, crystal){
-  	crystalDiv.append("<div><button type='button' class='btn' id=" + this.getID() + " value=" + this.getvalue() + "><img src=" + this.getSrc() + "></button></div>");
-
+  		crystalDiv.append("<div><button type='button' class='btn' id=" + this.getID() + 
+  		" value=" + this.getvalue() + "><img src=" + this.getSrc() + "></button></div>");
   	 });
  };
 
  function setRandomNumber() {
+ 	
  	randomNumber = (Math.floor(Math.random() * (maxRandomNumber - minRandomNumber + 1)) + minRandomNumber);
  	$("#randomNumber").html(randomNumber);
  };
 
 function addToScore(value) {
+	
 	totalScore += parseInt(value);
 	console.log(totalScore);
 	$("#totalScore").html(totalScore);
@@ -68,6 +90,7 @@ function addToScore(value) {
 };
 
 function resetScore() {
+	
 	totalScore = 0;
 	$("#totalScore").html(totalScore);
 };
@@ -92,6 +115,7 @@ function compareNumbers() {
 };
 
 function resetCrystalValues() {
+	
 	$.each(crystalArray, function(number, crystal) {
 		var tempValue = this.resetValue();
 		console.log(tempValue);
@@ -101,12 +125,14 @@ function resetCrystalValues() {
 };
 
 function startGame() {
+	
 	createCrystalArray();
 	displayCrystals();
 	setRandomNumber();
 };
 
 function resetGame() {
+	
 	resetCrystalValues();
 	setRandomNumber();
 	resetScore();
@@ -114,29 +140,10 @@ function resetGame() {
 
 	startGame();
 
+	$(".btn").click(function() { addToScore($(this).attr("value"));})
+
 	if (gameOver === true) {
 		resetGame();
 	}
-
-	$(".btn").click(function() { addToScore($(this).attr("value"));})
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
